710df56bc7133df822cd3d4a0f9972f92da83345
Gary Donovan <garyd@crucialfruit.com.au>
2011-09-29 09:50:05 +1000

    Version bump.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 ## Run Setup ##
 setup(
     name = "rhinoplasty",
-    version = "0.4.6",
+    version = "0.4.7",
     author = "Gary Donovan",
     author_email = "garyd@crucialfruit.com.au",
     url = "N/A", #FIXME get a URL
