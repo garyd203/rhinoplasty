4612efbae7fe1c0a51a256bd93e65a9717f0e40f
Gary Donovan <garyd@crucialfruit.com.au>
2011-09-29 12:30:20 +1000

    Include version information in package.

--- rhinoplasty/__init__.py
+++ rhinoplasty/__init__.py
@@ -1,1 +1,11 @@
 """Experimental extensions to Nose."""
+
+import re
+
+## Version Information ##
+
+# Version string
+from _version import __version__
+
+# Version tuple
+version_info = tuple(map(int, re.match("(\d+)\.(\d+)\.(\d+)", __version__).groups()))
