0addcb2b7b3e626537cf39d80e0ec211c5ebb399
Gary Donovan <garyd@crucialfruit.com.au>
2011-10-17 14:36:04 +1100

    Clean up test functions. Add new test functions.

--- tests/test_timed.py
+++ tests/test_timed.py
@@ -7,44 +7,96 @@ from nose.tools import TimeExpired
 from rhinoplasty.timed import timeboxed
 import random
 import time
-#FIXME from xtest import parametrise
 
 
 class TestTimeBoxedDecorator(object):
     """Test decorator that times out a method if it takes too long."""
     
-    #TODO tests:
-        # accept no args, sequential args, keyword args
-        # raise exception
-    
     # Test Methods
     # ------------
-    def test_timeout_triggered(self):
-        """A function that runs too long should trigger a timeout error."""
+    def test_function_with_no_parameters(self):
+        """Should be able to decorate a function that accepts no parameters."""
+        # Setup
         value = random.randint(0, 100000)
         
+        @timeboxed(1)
+        def func():
+            return value
+        
+        # Exercise
+        assert_equal(value, func())
+    
+    def test_function_with_multiple_parameters(self):
+        """Should be able to decorate a function that accepts sequential and
+        keyword  parameters.
+        """
+        # Setup
+        value_a = random.randint(0, 100000)
+        value_b = random.randint(0, 100000)
+        expected_result = "%s_%s" % (value_a, value_b)
+        
+        @timeboxed(1)
+        def func(a, b):
+            return "%s_%s" % (a, b)
+        
+        # Exercise
+        assert_equal(expected_result, func(value_a, b=value_b))
+    
+    def test_timeout_triggered(self):
+        """A function that runs too long should trigger a timeout error."""
+        # Setup
+        value = random.randint(0, 100000)
+        
+        @timeboxed(0.01)
+        def run_long_method(param):
+            time.sleep(10)
+            return param
+        
+        # Exercise
         with assert_raises(TimeExpired):
-            self._run_long_method(value)
+            run_long_method(value)
     
     def test_timeout_avoided(self):
         """A function that doesn't take too long should work normally."""
+        # Setup
         value = random.randint(0, 100000)
         
-        result = self._run_short_method(value)
+        @timeboxed(1)
+        def func(param):
+            return param
         
+        # Exercise
+        result = func(value)
+        
+        # Verify
         assert_equal(result, value, "Short run failed to return correct result")
+    
+    def test_exception_raised(self):
+        """An exception raised by the target function should be passed through to the caller."""
+        # Setup
+        class FrobnicationError(StandardError):
+            pass
+        
+        @timeboxed(1)
+        def func():
+            raise FrobnicationError()
+        
+        # Exercise
+        with assert_raises(FrobnicationError):
+            func()
     
     #FIXME turn test function into a generator
 #    @parametrise.function([
-#        0.01,
-#        0.1,
-#        1
+#        0.02,
+#        0.2,
+#        2,
 #    ])
 #    def test_timeout_length(self, time_period):
     def test_timeout_length(self):
         """The time out should not occur until the time period has elapsed."""
         # Setup
-        time_period = 1
+        time_period = 0.2
+        
         @timeboxed(time_period)
         def func():
             time.sleep(time_period*5)
@@ -58,18 +110,5 @@ class TestTimeBoxedDecorator(object):
         # Verify
         test_duration = end - start
         assert_greater_equal(test_duration, time_period)
-    
-    
-    # Helper Methods
-    # --------------
-    #TODO put these as local funcs
-    @timeboxed(0.01)
-    def _run_long_method(self, value):
-        time.sleep(10)
-        return value
-    
-    @timeboxed(1)
-    def _run_short_method(self, value):
-        return value
 
 
